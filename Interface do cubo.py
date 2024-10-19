5ee1ef7ea43b894b79fa7921321394994502792c
Davimmo <davi20bezerra@hotmail.com>
2024-10-18 23:58:33 -0300

    Pegando o código pronto do github da ursina

--- Interface do cubo.py
+++ Interface do cubo.py
@@ -1,63 +1,114 @@
 from ursina import *
 
-# Inicializar a aplicação Ursina
+
 app = Ursina()
 
-# Adicionar uma câmera que permite controle com o mouse
-editor_camera = EditorCamera(rotation_speed=200, panning_speed=20)
-distância=1.07
+cube_colors = [
+    color.blue,  # right
+    color.green,   # left
+    color.yellow,    # top
+    color.white,   # bottom
+    color.red,    # back
+    color.orange,    # front
+]
 
-# Função para criar um cubo colorido
-def create_colored_cube(x, y, z):
-    cube = Entity(scale=(1, 1, 1), position=(x, y, z))
+# make a model with a separate color on each face
+combine_parent = Entity(enabled=False)
+for i in range(3):
+    dir = Vec3(0,0,0)
+    dir[i] = 1
 
-    # Criar faces do cubo
-    if y==1*distância:
-        # Face de cima - amarelo
-        top_face = Entity(parent=cube, model='quad', color=color.yellow, position=(0, 0.5, 0), rotation_x=-90, scale=(1, 1), double_sided=True)
+    e = Entity(parent=combine_parent, model='plane', origin_y=-.5, texture='white_cube', color=cube_colors[i*2])
+    e.look_at(dir, 'up')
+
+    e_flipped = Entity(parent=combine_parent, model='plane', origin_y=-.5, texture='white_cube', color=cube_colors[(i*2)+1])
+    e_flipped.look_at(-dir, 'up')
+
+combine_parent.combine()
+
+
+# place 3x3x3 cubes
+cubes = []
+for x in range(3):
+    for y in range(3):
+        for z in range(3):
+            e = Entity(model=copy(combine_parent.model), position=Vec3(x,y,z) - (Vec3(3,3,3)/3), texture='white_cube')
+            cubes.append(e)
+
+
+# rotate a side when we click on it
+collider = Entity(model='cube', scale=3, collider='box', visible=False)
+
+def collider_input(key):
+    if mouse.hovered_entity == collider:
+        if key == 'left mouse down':
+            rotate_side(mouse.normal, 1)
+        elif key == 'right mouse down':
+            rotate_side(mouse.normal, -1)
+
+collider.input = collider_input
+
+
+rotation_helper = Entity()
+
+
+def rotate_side(normal, direction=1, speed=1):
+    if normal == Vec3(1,0,0):
+        [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.x > 0]
+        rotation_helper.animate('rotation_x', 90 * direction, duration=.15*speed, curve=curve.linear, interrupt='finish')
+    elif normal == Vec3(-1,0,0):
+        [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.x < 0]
+        rotation_helper.animate('rotation_x', -90 * direction, duration=.15*speed, curve=curve.linear, interrupt='finish')
+
+    elif normal == Vec3(0,1,0):
+        [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.y > 0]
+        rotation_helper.animate('rotation_y', 90 * direction, duration=.15*speed, curve=curve.linear, interrupt='finish')
+    elif normal == Vec3(0,-1,0):
+        [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.y < 0]
+        rotation_helper.animate('rotation_y', -90 * direction, duration=.15*speed, curve=curve.linear, interrupt='finish')
+
+    elif normal == Vec3(0,0,1):
+        [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.z > 0]
+        rotation_helper.animate('rotation_z', -90 * direction, duration=.15*speed, curve=curve.linear, interrupt='finish')
+    elif normal == Vec3(0,0,-1):
+        [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.z < 0]
+        rotation_helper.animate('rotation_z', 90 * direction, duration=.15*speed, curve=curve.linear, interrupt='finish')
+
+
+    invoke(reset_rotation_helper, delay=.2*speed)
+
+    if speed:
+        collider.ignore_input = True
+        @after(.25*speed)
+        def _():
+            collider.ignore_input = False
+            check_for_win()
+
+
+def reset_rotation_helper():
+    [setattr(e, 'world_parent', scene) for e in cubes]
+    rotation_helper.rotation = (0,0,0)
+
+
+win_text_entity = Text(y=.35, text='', color=color.green, origin=(0,0), scale=3)
+
+def check_for_win():
+    if {e.world_rotation for e in cubes} == {Vec3(0,0,0)}:
+        win_text_entity.text = 'SOLVED!'
+        win_text_entity.appear()
     else:
-        top_face = Entity(parent=cube, model='quad', color=color.black, position=(0, 0.5, 0), rotation_x=-90, scale=(1, 1), double_sided=True)
-    
-    if y==-1*distância:
-        # Face de baixo - branco
-        bottom_face = Entity(parent=cube, model='quad', color=color.white, position=(0, -0.5, 0), rotation_x=90, scale=(1, 1), double_sided=True)
-    else:
-        top_face = Entity(parent=cube, model='quad', color=color.black, position=(0,-0.5, 0), rotation_x=90, scale=(1, 1), double_sided=True)
-        
-
-    if z==-1*distância:
-        # Face da frente - laranja
-        front_face = Entity(parent=cube, model='quad', color=color.orange, position=(0, 0, -0.5), rotation_y=180, scale=(1, 1), double_sided=True)
-    else:
-        top_face = Entity(parent=cube, model='quad', color=color.black, position=(0, 0, -0.5), rotation_y=180, scale=(1, 1), double_sided=True)
-        
-
-    if z==1*distância:
-        # Face de trás - vermelho
-        back_face = Entity(parent=cube, model='quad', color=color.red, position=(0, 0, 0.5), scale=(1, 1), double_sided=True)
-    else:
-        top_face = Entity(parent=cube, model='quad', color=color.black, position=(0, 0, 0.5), scale=(1, 1), double_sided=True)
-        
-
-    if x==1*distância:
-        #Face da direita - azul
-        right_face = Entity(parent=cube, model='quad', color=color.blue, position=(0.5, 0, 0), rotation_y=-90, scale=(1, 1), double_sided=True)
-    else:
-        top_face = Entity(parent=cube, model='quad', color=color.black, position=(0.5, 0, 0), rotation_y=-90, scale=(1, 1), double_sided=True)
+        win_text_entity.text = ''
 
 
-    if x==-1*distância:
-        left_face = Entity(parent=cube, model='quad', color=color.green, position=(-0.5, 0, 0), rotation_y=90, scale=(1, 1), double_sided=True)
-        # Face da esquerda - verde
-    else:
-        top_face = Entity(parent=cube, model='quad', color=color.black, position=(-0.5, 0, 0), rotation_y=90, scale=(1, 1), double_sided=True)
+def randomize():
+    faces = (Vec3(1,0,0), Vec3(0,1,0), Vec3(0,0,1), Vec3(-1,0,0), Vec3(0,-1,0), Vec3(0,0,-1))
+    for i in range(20):
+        rotate_side(normal=random.choice(faces), direction=random.choice((-1,1)), speed=0)
 
+randomize_button = Button(text='randomize', color=color.azure, position=(.7,-.4), on_click=randomize)
+randomize_button.fit_to_text()
 
-# Criar os cubos em uma grade 3x3x3
-for x in range(-1, 2):  # x=-1, 0, 1
-    for y in range(-1, 2):  # y=-1, 0, 1
-        for z in range(-1, 2):  # z=-1, 0, 1
-            create_colored_cube(x * distância, y * distância, z * distância)  # Multiplicando por 2 para espaçar os cubos
+window.color = color._16
+EditorCamera()
 
-# Executar a aplicação
 app.run()
